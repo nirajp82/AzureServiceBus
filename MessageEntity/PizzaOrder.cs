a30f327106dc6f9c10247029386939bf34da4f9a
Niraj Patel <nirajp82@yahoo.com>
2021-02-21 18:24:46 -0500

    Added Message Entity Folder

---
+++ MessageEntity/PizzaOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MessageEntity
+{
+    public class PizzaOrder
+    {
+        public string CustomerName { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+        public override string ToString()
+        {
+            return $"CustomerName: {CustomerName} Type:{Type} Size:{Size}";
+        }
+    }
+}
---
+++ MessageEntity/RfidTag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageEntity
+{
+    public class RfidTag
+    {
+        public string TagId { get; private set; }
+        public string Product { get; set; }
+        public double Price { get;  set; }
+
+        public RfidTag()
+        {
+            TagId = Guid.NewGuid().ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"Product: {Product} \t Price:{Price}";
+        }
+    }
+}
